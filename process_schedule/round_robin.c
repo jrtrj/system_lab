67b12bdbd96c3ed931a3a8cfcdf00795dec678e1
jrtrj <jerit.reji.dev@gmail.com>
2025-10-13 21:55:11 -0400

    round robin

---
+++ process_schedule/round_robin.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 20
+
+struct Process {
+	int pid;
+	int at;
+	int bt;
+	int ct;
+	int tat;
+	int wt;
+	bool completed;
+};
+
+// insertion sort
+void sort(struct Process process[],int size) {
+    for(int j = 1; j < size; j++){
+        struct Process key = process[j];
+        int i = j-1;
+        while(i > -1 && process[i].at > key.at) {
+            process[i+1] = process[i];
+            i--;
+        }
+        process[i+1] = key;
+    }
+}
+ 
+int main() {
+	struct Process process[MAX];
+	int no_of_process;
+    int time_quantum;
+	float avg_wt,avg_tat;
+ 
+	printf("Enter the number of processes\n");
+	scanf("%d",&no_of_process);
+ 
+	printf("Enter the time quantum\n");
+	scanf("%d",&time_quantum);
+
+	for(int i = 0; i < no_of_process; i++) {
+		process[i].pid = i+1;
+        process[i].completed = false;
+	}
+ 
+	printf("Enter the arrival time and burst time for processes\n");
+	for(int i = 0; i < no_of_process; i++) {
+		scanf("%d",&process[i].at);	
+		scanf("%d",&process[i].bt);	
+	}
+ 
+	sort(process,no_of_process);
+
+	int current_time = 0;
+	int completed = 0;
+    struct Process ready_queue[100];
+
+    while(completed < no_of_process) {
+       for(int i = 0; i < no_of_process; i++) {
+            if(process[i].at <= current_time && 
+                    process[i].completed == false) {
+                        ready_queue[i] = process[i];
+               }
+            }
+       }
+       if(idx != -1) {
+            process[idx].ct = process[idx].bt + current_time;
+            process[idx].tat = process[idx].ct - process[idx].at;
+            process[idx].wt = process[idx].tat - process[idx].bt;
+            process[idx].completed = true;
+            current_time += process[idx].bt;
+            completed++;
+            //for gantt chart
+            for(int i = 0; i<process[idx].bt; i++){
+                execution_log[log_index++] = process[idx].pid;
+            }
+       }
+       else {
+            current_time++;
+       }
+
+    }
+
+	int sum_tat = 0;
+	int sum_wt = 0;
+	for(int i = 0; i < no_of_process;i++) {
+		sum_tat += process[i].tat;
+		sum_wt += process[i].wt; 
+	}
+	avg_tat = (float)sum_tat/no_of_process;
+	avg_wt = (float)sum_wt/no_of_process;
+ 
+	printf("Pid\tat\tbt\tct\ttat\twt");
+	for(int i=0;i<no_of_process;i++){
+		printf("\n%d\t%d\t%d\t%d\t%d\t%d",
+                process[i].pid,process[i].at,process[i].bt,process[i].ct,process[i].tat,process[i].wt);
+	}
+	printf("\n\naverage turnaround time = %.2f",avg_tat);
+	printf("\naverage waiting time = %.2f",avg_wt);
+
+    //printing gantt chart
+    printf("\nGantt Chart\n");
+    for(int i = 0;i < log_index;i++) {
+        printf("P%d|",execution_log[i]);
+    }
+    printf("\n");
+    return 0;
+}
